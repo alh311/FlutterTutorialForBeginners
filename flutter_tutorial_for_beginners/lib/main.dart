e4b6938d3ad81f08d55ae2bc06e34c771422abfe
Alex Hutchins <alh311@gmail.com>
2022-08-27 21:09:22 -0600

    Adds Home page

--- flutter_tutorial_for_beginners/lib/main.dart
+++ flutter_tutorial_for_beginners/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial_for_beginners/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,6 +36,7 @@ class _RootPageState extends State<RootPage> {
       appBar: AppBar(
         title: const Text('Flutter'),
       ),
+      body: const HomePage(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           debugPrint('Floating Action Button');
